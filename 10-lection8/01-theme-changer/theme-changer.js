65c3dfe3c2fa3416e33c86313ba52e6973fd9b8f
Aliaksei Charniauski <charniauski.aliaksei@gmail.com>
2021-06-18 23:13:10 +0300

    dark-theme-main-commit

--- 10-lection8/01-theme-changer/theme-changer.js
+++ 10-lection8/01-theme-changer/theme-changer.js
@@ -1,6 +1,22 @@
 const changer = document.getElementById('theme-changer');
+const changer2 = document.getElementById('theme-changer2');
 const html = document.documentElement;
 
 changer.addEventListener('change', function() {
     html.toggleAttribute('data-theme-dark');
+    if (changer.checked) {
+        changer2.checked = true;
+    } else {
+        changer2.checked = false;
+    }
+});
+
+
+changer2.addEventListener('change', function() {
+    html.toggleAttribute('data-theme-dark');
+    if (changer2.checked) {
+        changer.checked = true;
+    } else {
+        changer.checked = false;
+    }
 });
